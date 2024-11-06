71fc74d5547745fe06b8b9a89c1640d4ad07267f
浅梦 <867583927@qq.com>
2024-11-06 22:33:22 +0800

    page接口

--- src/pages/find/Find.vue
+++ src/pages/find/Find.vue
@@ -1,5 +1,20 @@
 <template>
-  <view>发现</view>
+  <view class="tabs">
+    <view class="tab-list">
+      <button
+        v-for="tab in tabs"
+        :key="tab"
+        :class="{ active: currentTab === tab }"
+        @click="currentTab = tab"
+      >
+        {{ tab }}
+      </button>
+    </view>
+    <view class="tab-content">
+      <navigator :is="currentTabComponent" />
+    </view>
+  </view>
+
   <view class="con">
     <swiper class="swiper" autoplay interval="3000" duration="500">
       <swiper-item v-for="item in banners" :key="item.targetId" @click="goDetail(item)">
@@ -10,8 +25,8 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
-import { getBannerApi } from '../../services/index'
+import { ref, onMounted, computed } from 'vue'
+import { getBannerApi, getPageApi } from '../../services/index'
 
 const banners = ref([])
 
@@ -22,6 +37,31 @@ onMounted(async () => {
     banners.value = res.data.banners
   } catch (error) {
     console.error(error)
+  }
+  try {
+    const res = await getPageApi()
+    console.log(res)
+    // banners.value = res.data.banners
+  } catch (error) {
+    console.error(error)
+  }
+})
+
+const tabs = ['音乐', '博客', '听书', '直播']
+const currentTab = ref('音乐');
+
+const currentTabComponent = computed(() => {
+  switch (currentTab.value) {
+    case '音乐':
+      return 'MusicComponent'
+    case '博客':
+      return 'BlogComponent'
+    case '听书':
+      return 'AudiobookComponent'
+    case '直播':
+      return 'LiveComponent'
+    default:
+      return 'MusicComponent'
   }
 })
 </script>
@@ -36,4 +76,32 @@ onMounted(async () => {
   .img{
     width: 100%;
   }
+
+  .tabs {
+  display: flex;
+  flex-direction: column;
+}
+
+.tab-list {
+  display: flex;
+  border-bottom: 1px solid #ccc;
+}
+
+.tab-list button {
+  padding: 10px 20px;
+  border: none;
+  background: none;
+  cursor: pointer;
+  outline: none;
+}
+
+.tab-list button.active {
+  border-bottom: 2px solid blue;
+}
+
+.tab-content {
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-top: none;
+}
 </style>
--- src/services/index.ts
+++ src/services/index.ts
@@ -3,3 +3,7 @@ import request from './request'
 export const getBannerApi = () => {
   return request({ url: '/banner' })
 }
+
+export const getPageApi = () => {
+  return request({ url: '/homepage/block/page' })
+}
