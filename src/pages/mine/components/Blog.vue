67b9adb875581a99515ec47564336edbe3772e07
小瑞. <14986650+xiao-rui111@user.noreply.gitee.com>
2024-11-07 11:24:23 +0800

    修改了我的

---
+++ src/pages/mine/components/Blog.vue
@@ -0,0 +1,12 @@
+<template>
+<view>博客</view>
+</template>
+
+<style lang='scss' scoped>
+
+</style>
+
+<script setup>
+import { ref } from 'vue'
+
+</script>
---
+++ src/pages/mine/components/Like.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- 我喜欢区域 -->
+  <view class="bottom">
+    <view class="like">
+    <view class="like-title">我喜欢的音乐</view>
+    <view class="like-content">
+      <view class="like-item">
+          <view class="like-pic"></view>
+          <view class="like-desc">
+            <view class="like-name">音乐名称</view>
+            <view class="like-artist">歌手</view>
+          </view>
+    </view>
+    </view>
+    </view>
+</view>
+</template>
+
+<style lang='scss' scoped>
+.bottom{
+  height: calc(100vh - 528rpx);
+  background-color: #f4f4f4;
+}
+</style>
+
+<script setup>
+import { ref } from 'vue'
+
+</script>
