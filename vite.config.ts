c58cee0bca1fe91f75719285badb54b2a6a0ba38
浅梦 <867583927@qq.com>
2024-11-08 11:56:39 +0800

    打包

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,28 @@
 import { defineConfig } from "vite"
 import uni from "@dcloudio/vite-plugin-uni"
 
+const INVALID_CHARS_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g
+const DRIVE_LETTER_REGEX = /^[a-zA-Z]:/i
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [uni()],
-  server: {
-    hmr: true,  // 确保热重载开启
+  base: '/WYCloud',
+  // server: {
+  //   hmr: true,  // 确保热重载开启
+  // }
+  build: {
+    rollupOptions: {
+      output: {
+        sanitizeFileName(name) {
+          const match = DRIVE_LETTER_REGEX.exec(name)
+          const driveLetter = match ? match[0] : ''
+          return (
+            driveLetter +
+            name.slice(driveLetter.length).replace(INVALID_CHARS_REGEX, '')
+          )
+        }
+      }
+    }
   }
 })
