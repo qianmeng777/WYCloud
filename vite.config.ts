7f2a04b22f610cc9fb4022a90042f943ee0e1b8b
邸泽扬 <849905099@qq.com>
2024-11-09 15:01:09 +0800

    底部播放器列表可点击打开关闭

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite"
 import uni from "@dcloudio/vite-plugin-uni"
-
+import path from 'path';
 const INVALID_CHARS_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g
 const DRIVE_LETTER_REGEX = /^[a-zA-Z]:/i
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // resolve: {
+  //   alias: {
+  //     '@': path.resolve(__dirname, 'src'),
+  //     '@dcloudio': path.resolve(__dirname, 'node_modules/@dcloudio')
+  //   }
+  // },
   plugins: [uni()],
   base: '/WYCloud',
   // server: {
